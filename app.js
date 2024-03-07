86d60979db70edabc41b484afff34803ac491030
albi nur <albi27076@gmail.com>
2024-03-07 23:57:10 +0700

    quiz

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var kategoriRouter = require('./routes/kategori');
+var mahasiswaRouter = require('./routes/mahasiswa');
+var pendidikanRouter = require('./routes/pendidikan');
+var keahlianRouter = require('./routes/keahlian');
 
 var app = express();
 
@@ -40,6 +43,9 @@ app.use(flash()); // gunakan flashh express menggunakan use
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/kategori', kategoriRouter);
+app.use('/mahasiswa', mahasiswaRouter);
+app.use('/pendidikan', pendidikanRouter);
+app.use('/keahlian', keahlianRouter);
 
 app.use('/', indexRouter);
 
